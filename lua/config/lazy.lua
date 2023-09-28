f23d0dacd3dfe213d9dd32693bc835f6856623fc
waves <986299679@qq.com>
2023-09-28 20:44:44 +0800

    feat: add project manager

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap" },
     -- import colors
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import test
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
