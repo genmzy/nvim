de6b2e63b40a612ae338811f68391ce8f54e99d7
genmzy <986299679@qq.com>
2024-02-06 11:11:56 +0800

    feat(statusline): lualine use another icon for branch of section_b

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -2,10 +2,18 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      component_separators = { left = "", right = "" },
+      component_separators = {
+        left = "",
+        right = "",
+      },
       section_separators = {
         left = "",
         right = "",
+      },
+    },
+    sections = {
+      lualine_b = {
+        { "branch", icon = { "" } },
       },
     },
   },
