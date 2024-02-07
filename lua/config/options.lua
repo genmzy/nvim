fbc22f10959e85c220ab5e99b60f25795b10ad82
genmzy <986299679@qq.com>
2024-02-07 09:58:11 +0800

    fix(options): encodings for Chinese

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -31,3 +31,7 @@ opt.guicursor = "a:blinkon800"
 -- cursor
 opt.guicursor =
   "n-v-c:underline,i-ci-ve:ver40,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+opt.fileencodings = "utf-8,gb2312,gb18030,gbk,ucs-bom,cp936,latin1"
+opt.enc = "utf-8"
+opt.fencs = "utf-8,gbk,gb2312,gb18030"
