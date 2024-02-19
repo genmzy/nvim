827b5d57f5e079b92e431f018f394eb75d081df2
genmzy <986299679@qq.com>
2024-02-19 10:49:54 +0800

    feat(keymap): add marks plugin and marks keymap

---
+++ lua/plugins/marks.lua
@@ -0,0 +1,14 @@
+return {
+  "chentoast/marks.nvim",
+  opts = {
+    default_mappings = false,
+  },
+  lazy = false,
+  keys = {
+    { [[<leader>m]], mode = { "n", "x" }, desc = "+marks" },
+    { [[<leader>mm]], mode = { "n", "x" }, "<Plug>(Marks-toggle)", desc = "Mark line" },
+    { [[<leader>mb]], mode = { "n", "x" }, "<Plug>(Marks-deletebuf)", desc = "Delete buffer marks" },
+    { [[<leader>mn]], mode = { "n", "x" }, "<Plug>(Marks-next)", desc = "Next mark" },
+    { [[<leader>mN]], mode = { "n", "x" }, "<Plug>(Marks-prev)", desc = "Prev mark" },
+  },
+}
