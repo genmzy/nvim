dd0e29e9221c086b3d5666bef5e13dafee85a401
genmzy <986299679@qq.com>
2024-05-17 17:53:55 +0800

    feat: side bar transparent

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,12 @@
+local linux_and_dark = vim.loop.os_uname().sysname == "Linux" and vim.o.background == "dark"
+
 return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = {
-      transparent = vim.loop.os_uname().sysname == "Linux" and vim.o.background == "dark" or false,
+      transparent = linux_and_dark,
+      styles = { sidebars = linux_and_dark and "transparent" or "dark" },
     },
   },
 }
