e6cdbfd108f7c9a0b0bce81f9b66a19a5fb4e815
genmzy <986299679@qq.com>
2024-05-19 18:31:59 +0800

    feat: revert transparent

--- lua/plugins/colors.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-      transparent = vim.loop.os_uname().sysname == "Linux" and vim.o.background == "dark" or false,
-    },
-  },
-}
