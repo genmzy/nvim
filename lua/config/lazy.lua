1bf8cd6b4ba59281f8a554864b3f6ebefb44460d
genmzy <986299679@qq.com>
2024-06-11 20:24:39 +0800

    feat: use lazyvim sql extras instead of native dadbod and dadbod-ui

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     -- import debugger
     { import = "lazyvim.plugins.extras.dap" },
     -- import colors
