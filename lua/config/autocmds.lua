13ab2aa958cfe383f0e7c1c2bd6d8ba16af834fb
genmzy <986299679@qq.com>
2024-07-07 12:53:22 +0800

    feat: use default cword highlight

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,6 +5,8 @@
 local hl = vim.api.nvim_set_hl
 local hl_underline = { bg = "NONE", underline = true }
 
+--[[
+
 local hl_set_underline = function(s)
   hl(0, s, hl_underline)
 end
@@ -13,5 +15,7 @@ end
 hl_set_underline("LspReferenceRead")
 hl_set_underline("LspReferenceText")
 hl_set_underline("LspReferenceWrite")
+
+]]
 
 hl(0, "LspInlayHint", { fg = "#545c7e", underdashed = true })
