59a8cdbf806e667a5a4693700e8c19074e41bac9
genmzy <986299679@qq.com>
2024-08-07 00:12:07 +0800

    fix: some dracula colors
    temp
    
    fix: some dracula colors

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+--
+-- dracula
+--
+
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    config = {
+      italic_comment = true,
+      overrides = {
+        NormalFloat = { bg = "#313244" },
+        StatusLine = { bg = "#44475a" },
+        LspReferenceRead = { bold = true },
+        LspReferenceText = { bold = true },
+        LspReferenceWrite = { bold = true },
+        PmenuSel = { fg = "#21222c", bg = "#8be9fd", bold = true },
+        ["@punctuation"] = { fg = "#7f849c" },
+        ["@punctuation.bracket"] = { fg = "#7f849c" },
+        ["@punctuation.delimiter"] = { fg = "#7f849c" },
+        ["@function"] = { fg = "#69ff94" },
+        ["@function.method"] = { fg = "#69ff94" },
+        ["@function.macro"] = { fg = "#69ff94" },
+        ["@operator"] = { fg = "#a4ffff" },
+      },
+    },
+  },
+
+  -- Configure LazyVim to load dracula
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
