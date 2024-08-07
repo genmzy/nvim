8b61e62103f935d186006b4a4b535cc7fa04e3ce
genmzy <986299679@qq.com>
2024-08-07 21:40:42 +0800

    feat: first init

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+--
+-- rose-pine
+--
+
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = { bold = true, italic = false },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-moon",
+    },
+  },
+}
