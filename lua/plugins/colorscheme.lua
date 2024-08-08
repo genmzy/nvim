e3cb1c92c940e2859f6681570c60d069a42d2095
genmzy <986299679@qq.com>
2024-08-09 01:17:27 +0800

    feat: init for deus

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+--
+-- everforest
+--
+
+return {
+  {
+    "genmzy/deus.nvim",
+    opts = { disable_italic_comments = true },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "deus",
+    },
+  },
+}
