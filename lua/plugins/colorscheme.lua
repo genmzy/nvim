6ec6c1a571b4cd0cd2e62acd97a26116ed0d6f9d
genmzy <986299679@qq.com>
2024-08-09 23:32:59 +0800

    feat: change current word to bg1 of deus

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,9 +8,11 @@ return {
     opts = {
       hard = true,
       disable_italic_comments = true,
+      on_highlights = function(hl, p)
+        hl.CurrentWord = { bg = p.bg1 }
+      end,
     },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
