5e90c90998fcc641ad3171baba8ea70cb58a00fc
genmzy <986299679@qq.com>
2024-12-22 18:48:56 +0800

    feat: add local color configuration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
     "genmzy/deus.nvim",
     opts = {
       hard = true,
-      disable_italic_comments = true,
+      disable_italic_comments = require("local") and not require("local").italic.comments or false,
       on_highlights = function(hl, p)
         hl.CurrentWord = { bg = p.bg1 }
       end,
