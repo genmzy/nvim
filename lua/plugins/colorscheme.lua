c44bc4b49f8194d628698b533eb1c5d9de7defa3
genmzy <986299679@qq.com>
2025-03-14 20:55:32 +0800

    feat: update color
    temp

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,17 @@
 --
 
 return {
-  { "Shatur/neovim-ayu" },
+  {
+    "Shatur/neovim-ayu",
+    config = function()
+      require("ayu").setup({
+        overrides = {
+          Function = { fg = "#dfbfff" },
+          ["@lsp.type.parameter"] = { fg = "#95e6cb" },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
