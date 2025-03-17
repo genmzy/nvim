84e753a7ba64da9c45b427f459dc21140643f176
genmzy <986299679@qq.com>
2025-03-17 09:42:48 +0800

    feat: update for ayu color overrides

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,15 +7,18 @@ return {
     "Shatur/neovim-ayu",
     config = function()
       require("ayu").setup({
-        overrides = vim.opt.background._value == "dark" and {
-          Function = { fg = "#dfbfff" },
-          ["@lsp.type.variable"] = {},
-          ["@lsp.type.variable.go"] = {},
-          ["@lsp.type.parameter.go"] = { fg = "#f28779" },
-          ["@variable.parameter.go"] = { fg = "#f28779" },
-          ["@variable.go"] = { fg = "#95e6cb" },
-          ["@property.go"] = { fg = "#5ccfe6" },
-        } or {},
+        overrides = vim.opt.background._value == "dark"
+            and {
+              Function = { fg = "#95e6cb" },
+              ["@lsp.type.variable"] = {},
+              ["@lsp.type.variable.go"] = {},
+              ["@variable.go"] = {},
+              ["@property.go"] = { fg = "#5ccfe6" },
+              -- for all
+              ["@lsp.type.parameter"] = { fg = "#dfbfff" },
+              ["@variable.parameter"] = { fg = "#f28779" },
+            }
+          or {},
       })
     end,
   },
