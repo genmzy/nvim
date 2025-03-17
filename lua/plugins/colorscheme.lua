60729703ea9c18ad2d0ad9661fcc97d82bafa60c
genmzy <986299679@qq.com>
2025-03-17 09:24:12 +0800

    update: color for current go

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,10 +7,15 @@ return {
     "Shatur/neovim-ayu",
     config = function()
       require("ayu").setup({
-        overrides = {
+        overrides = vim.opt.background._value == "dark" and {
           Function = { fg = "#dfbfff" },
-          ["@lsp.type.parameter"] = { fg = "#95e6cb" },
-        },
+          ["@lsp.type.variable"] = {},
+          ["@lsp.type.variable.go"] = {},
+          ["@lsp.type.parameter.go"] = { fg = "#f28779" },
+          ["@variable.parameter.go"] = { fg = "#f28779" },
+          ["@variable.go"] = { fg = "#95e6cb" },
+          ["@property.go"] = { fg = "#5ccfe6" },
+        } or {},
       })
     end,
   },
