0f14f4069df6163bc7168d98b18f17c204e39214
genmzy <986299679@qq.com>
2025-04-13 10:33:01 +0800

    fix(color): unnecessary merge

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,8 +16,5 @@ return {
     opts = {
       colorscheme = "rose-pine-dawn",
     },
-    on_highlights = function(hl, c)
-      hl["@lsp.type.namespace.go"] = { fg = "#86e1fc" }
-    end,
   },
 }
