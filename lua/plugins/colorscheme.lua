90efff6344748519c35b86953478f68f9ca340fc
genmzy <986299679@qq.com>
2025-06-02 12:23:05 +0800

    feat: change require

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,14 @@
 -- everforest
 --
 
+local ok, lcfg = pcall(require, "local")
+
 return {
   {
     "genmzy/deus.nvim",
     opts = {
       hard = true,
-      disable_italic_comments = require("local") and not require("local").italic.comments or false,
+      disable_italic_comments = ok and not lcfg.italic.comments or false,
       on_highlights = function(hl, p)
         hl.CurrentWord = { bg = p.bg1 }
       end,
