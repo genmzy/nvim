732b7261838158a7455de650ca4f91d082c7f30b
genmzy <986299679@qq.com>
2025-06-11 09:41:28 +0800

    feat: add color aurora

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,14 +4,18 @@
 
 return {
   {
-    "genmzy/nvim-snazzy",
-    dependencies = { "rktjmp/lush.nvim" },
+    "ray-x/aurora",
+    init = function()
+      vim.g.aurora_italic = 0
+      -- vim.g.aurora_transparent = 0
+      vim.g.aurora_bold = 0
+    end,
   },
   -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "snazzy",
+      colorscheme = "aurora",
     },
   },
 }
