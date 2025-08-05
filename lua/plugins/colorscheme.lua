96740eeb3f9a36121d82c1505d29e1a72e7c43bb
genmzy <986299679@qq.com>
2025-08-05 20:38:01 +0800

    feat: follow trans override color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,7 +10,15 @@ return {
       keywords = { italic = false },
     },
     on_highlights = function(hl, c)
-      hl["@lsp.type.namespace.go"] = { fg = "#86e1fc" }
+      hl["@lsp.type.namespace.go"] = { fg = c.cyan }
+      hl["@lsp.type.namespace.zig"] = { fg = c.cyan }
+      hl["@lsp.type.string.zig"] = {}
+      hl["BlinkCmpLabelMatch"] = { fg = c.teal }
+      hl["@label"] = { fg = c.magenta }
+      hl["@string.escape"] = { fg = c.purple }
+      hl["@lsp.type.string.go"] = {}
+      hl["@character.printf"] = { fg = c.magenta }
+      hl["@constant.builtin"] = { fg = c.orange }
     end,
   },
 }
